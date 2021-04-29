2adb136bf6ab12fb1ed0d42f2e98764ff3ae2460
Abinay-2021 <83108428+Abinay-2021@users.noreply.github.com>
2021-04-29 21:08:23 +0530

    Update DuplicateLead.java

--- DuplicateLead.java
+++ DuplicateLead.java
@@ -9,79 +9,59 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DuplicateLead {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-WebDriverManager.chromedriver().setup();
-		
-		ChromeDriver driver = new ChromeDriver ();
-		
+		WebDriverManager.chromedriver().setup();
+
+		ChromeDriver driver = new ChromeDriver();
+
 		driver.get("http://leaftaps.com/opentaps/control/main");
-		
+
 		driver.manage().window().maximize();
-		 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
 		driver.findElement(By.id("password")).sendKeys("crmsfa");
 		driver.findElement(By.className("decorativeSubmit")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.findElement(By.linkText("Email")).click();
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("Raj@yahoo.com");
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		Thread.sleep(2000);
+
+		String text = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]/a")).getText();
+		System.out.println("Name of the User " + text);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-firstName'])[1]/a")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Duplicate Lead']")).click();
+		Thread.sleep(2000);
+
+		String text2 = driver.getTitle();
+
+		if (text2.equals("Duplicate Lead | opentaps CRM")) {
+
+			System.out.println("Title matchs as " + text2);
+		} else {
+			System.out.println("title doesnot match");
 		}
-		String text = driver.findElement(By.xpath("//table/tbody/tr[1]/td[3]/div/a ")).getText();
-		System.out.println("Name of the User "+text);
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		driver.findElement(By.xpath("//input[@value='Create Lead']")).click();
+		Thread.sleep(2000);
+		String text3 = driver.findElement(By.xpath("//span[@id='viewLead_firstName_sp']")).getText();
+
+		if (text.equals(text3)) {
+
+			System.out.println("Duplicate Lead name captured " + text3);
+		} else {
+			System.out.println("duplicated lead name is not same as captured name");
 		}
-        driver.findElement(By.xpath("(//table/tbody/tr/td[3]/div/a)[1]")).click();
-        try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-     driver.findElement(By.xpath("//a[text()='Duplicate Lead']")).click();
-     try {
-  			Thread.sleep(2000);
-  		} catch (InterruptedException e) {
-  			// TODO Auto-generated catch block
-  			e.printStackTrace();
-  		}
-    
-     String text2 =driver.getTitle();
-    
-     
-     if(text2.equals("Duplicate Lead | opentaps CRM")) {
-    	 
-    	 System.out.println("Title matchs as "+ text2);
-     }
-     else {
-    	 System.out.println("title doesnot match");
-     }
-		
-     driver.findElement(By.xpath("//input[@value='Create Lead']")).click();
-     driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-     String text3 = driver.findElement(By.xpath("//span[@id='viewLead_firstName_sp']")).getText();
-   
-     if (text.equals(text3)) {
-    	 
-    	  System.out.println("Duplicate Lead name captured "+ text3);
-     }
-     else {
-    	 System.out.println("duplicated lead name is not same as captured name");
-     }
-     
-     driver.close();
+
+		driver.close();
 	}
 
 }
