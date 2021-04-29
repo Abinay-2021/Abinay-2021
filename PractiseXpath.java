a83e1a3cc0d213533194d168746ca5d52501c40e
Abinay-2021 <83108428+Abinay-2021@users.noreply.github.com>
2021-04-29 21:06:26 +0530

    Update PractiseXpath.java

--- PractiseXpath.java
+++ PractiseXpath.java
@@ -5,39 +5,36 @@ import org.openqa.selenium.By;
 public class PractiseXpath {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
-		**No need to write Selenium script - Just find these elements in the DOM using only XPaths
 		
-		Try all the xpaths that you've learnt (basic, text-based, axes..)
-		1. Launch the URL
+		//1. Launch the URL
 		
-		2. Enter the username based on its label
+		//2. Enter the username based on its label
 		driver.findElement(By.xpath("//label[@for="username"]/following-sibling::input")).sendKeys("");
 		
-		3. Enter the password based on its label
+		//3. Enter the password based on its label
 		driver.findElement(By.xpath("//label[@for="password"]/following-sibling::input")).sendKeys("");
-		4. Click Login
+		//4. Click Login
 		driver.findElement(By.xpath("//input[@value='Login']")).click();
-		5. Click on CRMSFA link
+		//5. Click on CRMSFA link
 		driver.findElement(By.xpath("//*[@id='label']/a")).click();
-		6. Click on Leads link
+		//6. Click on Leads link
 		driver.findElement(By.xpath("//a[text()='Leads']")).click();
-		7. Click on Merge Leads link
+		//7. Click on Merge Leads link
 			driver.findElement(By.xpath("//a[text()='Merge Leads']")).click();
-		8. Click the first img icon
+		//8. Click the first img icon
 			driver.findElement(By.xpath("(//img[@src='/images/fieldlookup.gif'])[1]")).click();
-		9. Select the first resulting lead
-			driver.findElement(By.xpath("(//*[@class='x-grid3-cell-inner x-grid3-col-partyId']//descendant::a)[1]")).click();
-		10. Click the second img icon
-		driver.findElement(By.xpath("//*[@class='x-grid3-cell-inner x-grid3-col-partyId']//descendant::a)[2]")).click();
-		11. Select the second resulting lead
-				driver.findElement(By.xpath("//a[text()='Merge Leads']")).click();
-		12. Click on Merge Lead (submit) button
+		//9. Select the first resulting lead
+			driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a")).click();
+		//10. Click the second img icon
+		driver.findElement(By.xpath("(//img[@src='/images/fieldlookup.gif'])[2]")).click();
+		//11. Select the second resulting lead
+		driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[2]/a")).click();
+		
+		//12. Click on Merge Lead (submit) button
 			driver.findElement(By.xpath("//a[text()='Merge']")).click();
-		13. Get the company name text based on its label
+		//13. Get the company name text based on its label
 			driver.findElement(By.xpath("//span[@id='viewLead_companyName_sp']")).getText();
-		*/
+		
 	}
 
 }
